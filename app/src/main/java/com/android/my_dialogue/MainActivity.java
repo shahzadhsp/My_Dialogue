26fd48936d447cbbe5403faaf94366c0c47b0029
shahzadhsp <shezi495612@gmail.com.com>
2023-04-19 14:33:09 +0500

    Dialog Box In Flutter

---
+++ app/src/main/java/com/android/my_dialogue/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.my_dialogue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
